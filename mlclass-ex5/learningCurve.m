41a0b856ab3aa105be0f5855a58cddff4a353592
Shamir Alavi <alavi1223@hotmail.com>
2014-11-20 20:55:16 -0500

    final

--- mlclass-ex5/learningCurve.m
+++ mlclass-ex5/learningCurve.m
@@ -55,8 +55,8 @@ error_val   = zeros(m, 1);
 
 for i = 1:m,
 	theta = trainLinearReg(X(1:i,:), y(1:i), lambda);
-	error_train(i) = (0.5/m) * sum((X(1:i,:)*theta - y(1:i)).^2);
-	error_val(i) = (0.5/m) * sum((Xval*theta - yval).^2);
+	error_train(i) = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
+	error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);;
 end
 
 
